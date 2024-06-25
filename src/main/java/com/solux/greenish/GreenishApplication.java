a1e4910c48c749505e9389054143037c15473593
osjkate <osjkate@sookmyung.ac.kr>
2024-06-25 17:13:15 +0900

    first commit

---
+++ src/main/java/com/solux/greenish/GreenishApplication.java
@@ -0,0 +1,13 @@
+package com.solux.greenish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solux/greenish/GreenishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solux.greenish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
